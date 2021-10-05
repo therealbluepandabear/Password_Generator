cb526d5f6be04cef3739eea2d59ab0132874dd32
xTheCoderx <justaguyoney@gmail.com>
2021-10-05 15:23:57 +1300

    Added basic functionality of generating a password in a non-object-oriented fashion. The user can call the generatePassword(int length) method and specify the length of the password. The method will return a string.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <ctime>
+
+using namespace std;
+
+char generateRandomChar();
+string generatePassword(int length);
+string removeWhitespace(string str);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    srand ( time(NULL) );
+
+    cout << generatePassword(12) << endl;
     return 0;
+}
+
+string generatePassword(int length) {
+    string password;
+    for (int i = 0; i < length; ++i) {
+        password += generateRandomChar();
+    }
+    return removeWhitespace(password);
+}
+
+string removeWhitespace(string str) {
+    string newStr;
+    for (int i = 0; i < str.length(); ++i) {
+        if (str[i] != ' ') {
+            newStr += str[i];
+        }
+    }
+    return newStr;
+}
+
+char generateRandomChar() {
+    char lettersArr[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
+    char rand = lettersArr[std::rand() % std::size(lettersArr)];
+    return rand;
 }
